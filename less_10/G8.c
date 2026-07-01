236eb6e2d41588333de8146965081e3eb76a7601
bank45 <bank45@yandex.ru>
2026-07-01 22:05:24 +0300

    G1...G8

--- less_10/G8.c
+++ less_10/G8.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
 
-	
+int compare(const void *a,const void *b)
+{
+	return *(int*)a - *(int*)b;
+}	
 	
 int main(int argc, char **argv)
 {
@@ -9,10 +14,12 @@ FILE *f;
 FILE *fout;
 
 
-int n;
+int c;
+int number = 0;
 
+int num=0;
 int count=0;
-char number[100];
+int arr[1000]={0};
 //~ char res[]={};
 
 
@@ -27,18 +34,43 @@ if(!fout) return 1;
 if(!f) return 1;
 
 
-while(fscanf(f,"%d",&n)==1)
+while((c=fgetc(f))!=EOF)
 {
-		printf("c: %c ",n);
-		number[count]=n;
+	if(isdigit(c))
+	{
+		//~ fputc(c, fout);
+		number = number * 10 + (c - '0');
+		//~ printf("c: %c \n",c);
+
+		//~ fputc(c, fout);
+		num=1;
+	}else
+	{
+		if(num)
+		{
+		//~ fputc(' ', fout);
+		arr[count] = number;
+		//~ printf("number: %d \n",number);		
 		count++;
+		
+		num=0;
+		number=0;
+		}
+	}
+		//~ printf("c: %c ",n);
+		//~ number[count]=n;
+		//~ count++;
 }
 
+//~ size_t n = sizeof(arr) / sizeof(arr[0]);
+qsort(arr, count, sizeof(int), compare);
+
 for(int i=0;i<count;i++)
-	printf("c: %c ",number[i]);
+{
+	//~ printf("%d ",arr[i]);
+	fprintf(fout, "%d ", arr[i]);
 
-
-
+}
    fclose(f);
    fclose(fout);
 	return 0;
